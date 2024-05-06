93cedca5eef919c01c0620ed59374023219003c6
Natharagon <nathozimek@gmail.com>
2024-05-06 16:04:44 +0200

    [ADD] English translation

---
+++ script.js
@@ -0,0 +1,13 @@
+let language = "FR";
+function changeLanguage() {
+    if (language == "FR") {
+        document.styleSheets[0].rules.item(0).style.cssText = "display: block";
+        document.styleSheets[0].rules.item(1).style.cssText = "display: none";
+        language = "EN";
+    }
+    else {
+        document.styleSheets[0].rules.item(0).style.cssText = "display: none";
+        document.styleSheets[0].rules.item(1).style.cssText = "display: block";
+        language = "FR";
+    }
+}
